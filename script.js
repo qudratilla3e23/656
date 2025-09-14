437e2457fb7dda1d24b419ad3f04da5413164cdd
qudratilla3e23 <abdullayevqudrat224@gmail.com>
2025-09-15 00:06:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const h1=document.getElementById("h1")
+const plusBtn=document.getElementById('plus-btn')
+const body=document.getElementsByTagName('body')[0];
+
+
+plusBtn.addEventListener("click",()=>{
+    body.style.backgroundColor="red"
+})
